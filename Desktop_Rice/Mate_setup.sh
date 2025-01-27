9248bd30f66939f30cb8304698576c3e79dd6668
pr0ff3 <150958256+sPROFFEs@users.noreply.github.com>
2025-01-27 11:45:34 +0100

    Update Mate_setup.sh

--- Desktop_Rice/Mate_setup.sh
+++ Desktop_Rice/Mate_setup.sh
@@ -4,20 +4,85 @@ REAL_HOME=$(getent passwd $REAL_USER | cut -d: -f6)
 SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
 WALLPAPER_DIR="${REAL_HOME}/Wallpapers"
 
+# Function to select cursors
+select_cursors() {
+    echo "Select cursor pack:"
+    select cursor_pack in "M200" "Catppuccin Mocha Mauve"; do
+        case $cursor_pack in
+            "M200")
+                CURSOR_FILE="M200.tar.gz"
+                CURSOR_NAME="M200"
+                break
+                ;;
+            "Catppuccin Mocha Mauve")
+                CURSOR_FILE="catppuccin-mocha-mauve-cursors.zip"
+                CURSOR_NAME="Catppuccin-Mocha-Mauve"
+                break
+                ;;
+            *) 
+                echo "Invalid option. Please try again."
+                ;;
+        esac
+    done
+}
+
+# Function to select icon pack
+select_icons() {
+    echo "Select icon pack:"
+    select icon_pack in "Candy Icons" "We10X Black"; do
+        case $icon_pack in
+            "Candy Icons")
+                ICON_FILE="candy-icons.tar.xz"
+                ICON_NAME="Candy-icons"
+                break
+                ;;
+            "We10X Black")
+                ICON_FILE="We10X-black.tar.xz"
+                ICON_NAME="We10X-Black"
+                break
+                ;;
+            *) 
+                echo "Invalid option. Please try again."
+                ;;
+        esac
+    done
+}
+
+# Execute cursor and icon selection
+select_cursors
+select_icons
+
+# Continue with installation
 sudo apt update
-sudo apt install -y mate-desktop-environment mate-terminal dconf-cli mate-themes
+sudo apt install -y mate-desktop-environment mate-terminal dconf-cli mate-themes unzip
 
 sudo -u $REAL_USER mkdir -p ${REAL_HOME}/.themes/Dracula
 sudo -u $REAL_USER mkdir -p ${REAL_HOME}/.icons/{cursors,default}
-sudo -u $REAL_USER mkdir -p ${REAL_HOME}/.icons/Candy-icons
+sudo -u $REAL_USER mkdir -p "${REAL_HOME}/.icons/${ICON_NAME}"
 sudo -u $REAL_USER mkdir -p ${WALLPAPER_DIR}
 
 cd "${SCRIPT_DIR}"
-tar xf M200.tar.gz -C ${REAL_HOME}/.icons/
-tar xf candy-icons.tar.xz -C ${REAL_HOME}/.icons/
-ln -s ${REAL_HOME}/.icons/M200/index.theme ${REAL_HOME}/.icons/default/index.theme
-sudo update-alternatives --install /usr/share/icons/default/index.theme x-cursor-theme ${REAL_HOME}/.icons/M200/index.theme 20
-chown -R $REAL_USER:$REAL_USER ${REAL_HOME}/.icons/{M200,Candy-icons,default}
+
+# Handle cursors
+if [[ "$CURSOR_FILE" == "M200.tar.gz" ]]; then
+    tar xf "$CURSOR_FILE" -C ${REAL_HOME}/.icons/
+    ln -s ${REAL_HOME}/.icons/M200/index.theme ${REAL_HOME}/.icons/default/index.theme
+    sudo update-alternatives --install /usr/share/icons/default/index.theme x-cursor-theme ${REAL_HOME}/.icons/M200/index.theme 20
+elif [[ "$CURSOR_FILE" == "catppuccin-mocha-mauve-cursors.zip" ]]; then
+    unzip "$CURSOR_FILE" -d ${REAL_HOME}/.icons/Catppuccin-Mocha-Mauve
+    ln -s ${REAL_HOME}/.icons/Catppuccin-Mocha-Mauve/index.theme ${REAL_HOME}/.icons/default/index.theme
+    sudo update-alternatives --install /usr/share/icons/default/index.theme x-cursor-theme ${REAL_HOME}/.icons/Catppuccin-Mocha-Mauve/index.theme 20
+fi
+
+# Handle icon packs
+if [[ "$ICON_FILE" == "candy-icons.tar.xz" ]]; then
+    tar xf "$ICON_FILE" -C ${REAL_HOME}/.icons/
+elif [[ "$ICON_FILE" == "We10X-black.tar.xz" ]]; then
+    tar xf "$ICON_FILE" -C ${REAL_HOME}/.icons/
+fi
+
+# Change icon ownership
+chown -R $REAL_USER:$REAL_USER ${REAL_HOME}/.icons/
 
 sudo -u $REAL_USER mkdir -p ${REAL_HOME}/.config/autostart
 cat > ${REAL_HOME}/.config/autostart/dracula-theme.desktop << EOL
@@ -38,8 +103,6 @@ cd "${SCRIPT_DIR}"
 cp wallpaper_* "${WALLPAPER_DIR}/"
 chown -R $REAL_USER:$REAL_USER ${WALLPAPER_DIR}
 
-cd "${SCRIPT_DIR}"
-cp wallpaper_* "${WALLPAPER_DIR}/"
 
 cat > "${WALLPAPER_DIR}/wallpaper-slideshow.xml" << EOL
 <background>
@@ -148,13 +211,13 @@ chown $REAL_USER:$REAL_USER "${WALLPAPER_DIR}/wallpaper-slideshow.xml"
 cat > /tmp/mate-setup.sh << EOL
 #!/bin/bash
 sleep 5
-# Configurar tema Dracula
+# Configure Dracula theme
 dconf write /org/mate/desktop/interface/gtk-theme "'Dracula'"
 dconf write /org/mate/marco/general/theme "'Dracula'"
-dconf write /org/mate/desktop/interface/icon-theme "'Candy-icons'"
-dconf write /org/mate/desktop/interface/cursor-theme "'M200'"
+dconf write /org/mate/desktop/interface/icon-theme "'${ICON_NAME}'"
+dconf write /org/mate/desktop/interface/cursor-theme "'${CURSOR_NAME}'"
 
-# Configurar terminal
+# Terminal configuration
 dconf write /org/mate/terminal/profiles/default/background-color "'#282a36'"
 dconf write /org/mate/terminal/profiles/default/foreground-color "'#f8f8f2'"
 dconf write /org/mate/terminal/profiles/default/palette "['#262626', '#E356A7', '#42E66C', '#E4F34A', '#9B6BDF', '#E64747', '#75D7EC', '#EFA554', '#7A7A7A', '#FF79C6', '#50FA7B', '#F1FA8C', '#BD93F9', '#FF5555', '#8BE9FD', '#FFB86C']"
@@ -162,7 +225,7 @@ dconf write /org/mate/terminal/profiles/default/use-theme-colors "false"
 dconf write /org/mate/terminal/profiles/default/bold-color-same-as-fg "true"
 dconf write /org/mate/terminal/profiles/default/cursor-color "'#f8f8f2'"
 
-# Configurar fondo de pantalla con transiciones
+# Configure wallpaper with transitions
 dconf write /org/mate/desktop/background/picture-filename "'${WALLPAPER_DIR}/wallpaper-slideshow.xml'"
 dconf write /org/mate/desktop/background/picture-options "'zoom'"
 dconf write /org/mate/desktop/background/show-desktop-icons "true"
@@ -170,7 +233,7 @@ EOL
 chmod +x /tmp/mate-setup.sh
 sudo -u $REAL_USER /tmp/mate-setup.sh
 
-read -p "¿Do you want to close the session now to apply the changes? (y/n): " answer
+read -p "Do you want to close the session now to apply the changes? (y/n): " answer
 case $answer in
     [Yy]* ) pkill -KILL -u $REAL_USER;;
     [Nn]* ) echo "Remember to close the session manually to see the changes.";;
